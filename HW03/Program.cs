9a8711cf68facab281cc27f41ddb90314ea94f5a
Anastasiya <atassmorodina@gmail.com>
2023-02-05 18:46:09 +0300

    HW03 done

---
+++ HW03/Program.cs
@@ -0,0 +1,32 @@
+﻿/*
+Задача 52. Задайте двумерный массив из целых чисел. Найдите среднее 
+арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.
+*/
+int[,] array = new int[3, 4]; // создаем массив согласно условию задачи: 3 строки, 4 столбца
+Random random = new Random(); // рандомное заполнение массива
+Console.WriteLine("Исходный массив: ");
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = random.Next(1, 10);
+    Console.Write("{0} ", array[i, j]);
+    }
+    Console.WriteLine();
+    }
+
+for (int j = 0; j < array.GetLength(1); j++)
+{
+    double sum = 0;
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        sum += array[i, j];
+    }
+    Console.Write($"{Math.Round(sum / array.GetLength(0), 1)} ");
+}
+
