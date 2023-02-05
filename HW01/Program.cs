9a6a0de49f4db6232b0e4d2f97586c65329023e9
Anastasiya <atassmorodina@gmail.com>
2023-02-05 16:26:15 +0300

    HW01 done

---
+++ HW01/Program.cs
@@ -0,0 +1,63 @@
+﻿/*
+Задача 47. Задайте двумерный массив размером m×n, заполненный 
+случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+int GetNumber(string message)
+{
+    int result = 0;
+
+    while(true)
+    {
+        Console.WriteLine(message);
+
+        if(int.TryParse(Console.ReadLine(), out result) && result > 0)
+        {
+            break;
+        }
+        else
+        {
+            Console.WriteLine("Ввели не корректное число. Повторите ввод.");
+        }
+    }
+
+    return result;
+}
+
+double[,] InitMatrix(int rows, int columns)
+{
+    double[,] array = new double[rows, columns];
+    Random rand = new Random();
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            array[i, j] = Math.Round(rand.Next(-100, 101)*0.1, 1);
+        }
+    }
+
+    return array;
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+int countOfRows = GetNumber("Введите кол-во строк:");
+int countOfColumns = GetNumber("Введите кол-во столбцов:");
+Console.WriteLine();
+double[,] matrix = InitMatrix(countOfRows, countOfColumns);
+
+PrintMatrix(matrix);
